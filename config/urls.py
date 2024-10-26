e7f863e32993e8fa306ebbc39dc827e583630d1b
HMS-CV-Unicorn <swisscheese3777@gmail.com>
2024-10-26 15:19:37 +0900

    Update database settings and add new URLs and views
    This commit updates the database settings in the `config/settings.py` file to use SQLite3 as the default database. It also adds new URLs and views in the `config/urls.py` and `posts/views.py` files respectively. The new URLs include routes for Allauth, the home page, and serving media files. The new view `timeline` renders a timeline of posts in the `posts/timeline.html` template.
    
    New files:
    - Added `templates/base_generic.html` template file.
    - Added `templates/posts/timeline.html` template file.

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 from rest_framework import routers
 from users.views import UserViewSet, user_list
 from posts.views import PostViewSet, post_list
+from posts import views as post_views
+from django.conf import settings
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -15,4 +18,9 @@ urlpatterns = [
     path('api/allauth/', include('allauth.urls')),  # ソーシャル認証API
     path('users/', include('users.urls')),  # ユーザーのURL
     path('posts/', include('posts.urls')),  # 投稿のURL
+    path('accounts/', include('allauth.urls')),  # AllauthのURL
+    path('home/', post_views.timeline, name='timeline'), #ホーム画面
+    path('', post_views.timeline, name='home'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # ��ディアファイルのURL
